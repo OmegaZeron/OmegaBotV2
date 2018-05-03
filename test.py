29e27ad51ce94574ae8be856cd5a967278be084f
Greg <omegazeron@yahoo.com>
2018-05-03 09:38:17 -0700

    Added/updated some songs, added tests for possible ways to play
    Ideally will run in the background to not lose focus on what I'm doing, and will close when done. Not sure if possible with just the program. 80speak integration possible?

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import os
-os.system(".\\DECtalk\\DEC\\SongofStorms.EN")
-os.system(".\\DECtalk\\DEC\\KaeporaGaebora.EN")
+os.startfile(".\\DECtalk\\DEC\\SongofStorms.EN")
+# os.system(".\\DECtalk\\DEC\\KaeporaGaebora.EN")
